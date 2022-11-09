60b0e71a6df03a5b375d1c4462fa60b05342018d
Raúl A. Salinas Martínez <nacapulque@gmail.com>
2022-11-09 15:18:21 -0600

    waves

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,11 +1,29 @@
 // Define la precisión de bits de los números de punto flotante
-precision mediump float; 
+precision mediump float;
 
 uniform float uTime;
 
 varying vec2 vUv;
 
-void main() {
+void main(){
+  
+  vec2 uv = vUv;
+  
+  float fr = 350.0;
+  float speed = 10.0;
+  
+  
+  float wave = sin((uv.x * uv.y) * fr + uTime * speed) * 0.5 + 0.5;
+  
+  uv.x = 1.0 - uv.x;
+  float wave2 = cos((uv.y * uv.x) * fr + uTime* speed) * 0.5 + 0.5;
+  
+  
+  vec4 color = vec4(0.0);
+  color.r = wave;
+  color.g = wave2;
+  
+  
   // RGBA
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); 
+  gl_FragColor = color;
 }
